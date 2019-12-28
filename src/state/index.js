a27e1fb248ba95f7bbeb3500697a899cc734a806
Danny Michaelis <dmichaelis0@gmail.com>
2019-12-28 09:05:27 -0500

    create test diff

--- src/state/index.js
+++ src/state/index.js
@@ -4,6 +4,7 @@ import viewFilters from "../state/entities/viewFilters";
 import rewards from "../state/entities/rewardList";
 import bank from "../state/entities/bank";
 import { save as saveState } from "../dataConnection";
+import createUpdateCommand from "./createUpdateCommand";
 const todoApp = (state, action) => {
   if (action.type === "loadSaveDated") return { ...state, ...action.payload };
   try {
@@ -31,3 +32,35 @@ const todoApp = (state, action) => {
 };
 
 export default todoApp;
+setTimeout(() => {
+  const actions = [
+    {
+      tasks: {}
+    },
+    {
+      tasks: {
+        "0": {
+          completed: false,
+          value: 1,
+          id: 0,
+          text: "a"
+        }
+      }
+    }
+    // {
+    //   tasks: {
+    //     "0": {
+    //       completed: true,
+    //       value: 1,
+    //       id: 0,
+    //       text: "a"
+    //     }
+    //   }
+    // }
+  ].map((s, i, ss) => {
+    if (i + 1 === ss.length) return "";
+    return createUpdateCommand(s, ss[i + 1]);
+  });
+  console.log(actions);
+  Promise.all(actions).then(p => console.log(p));
+}, 1000);
